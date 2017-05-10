5eb40380bc99be9cdc086a7805a07d167bcb9dc8
Chris Magnuson <hourglasssand@gmail.com>
2017-05-10 16:15:15 -0400

    Initial commit

---
+++ AdobeUserManagementPowerShell.psm1
@@ -0,0 +1,9 @@
+﻿function New-AdobeUserManagmentSelfSignedCertificate {
+    param (
+        [Parameter(Mandatory)]$Password
+    )
+    $CertificateResult = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -Subject AdobeUserManagementPowerShell
+    $Result.Thumbprint
+    $PasswordSecureString = ConvertTo-SecureString -String $Password -Force -AsPlainText
+    Export-PfxCertificate -cert $CertificateResult -FilePath c:\temp\cert.pfx -Password $PasswordSecureString
+}
